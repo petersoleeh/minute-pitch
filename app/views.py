f73ea34f04f36005c412d5ddd31c65c24f817f94
peter soleeh <petersoleeh@gmail.com>
2017-10-30 10:53:05 +0300

    defining the different routes

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from app import app
+
+
+@app.route('/')
+def index():
+    '''
+    landing page
+    '''
+    title = 'Minute pitch'
+    return render_template('index.html',title =title)
+
+
+@app.route('/categories/<id>')
+def categories(id):
+    '''
+    new route that will display the contents of a specific category
+    '''
+    title = f'{id}'
+    return render_template('categories.html',id = id)
