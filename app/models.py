937285a30924d5aee5e6e3b08bf808db7d4d1798
peter soleeh <petersoleeh@gmail.com>
2017-10-31 11:07:04 +0300

    create class User

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from . import db
 class Category:
     '''
     define the different category objects
@@ -8,4 +9,10 @@ class Category:
         self.title = title
 
 class User(db.model):
-    
+    __tablename__= 'users'
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'User {self.username}'
