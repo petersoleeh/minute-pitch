4488b0456625174ea9406f541f6fbf9f2ec32227
peter soleeh <petersoleeh@gmail.com>
2017-10-31 11:05:20 +0300

    initialize blueprint and the db

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,30 @@
 from flask import Flask
-from .config import DevConfig
+from config import config_options
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 bootstrap = Bootstrap()
 db = SQLAlchemy
 
-#initialize the application
-app = Flask(__name__)
+def create_app(config_name):
 
-#set up the configuration
-app.config.from_object(DevConfig)
-#app.config.from_pyfile("config.py")
 
-#initialize flask extensions
-bootstrap.init_app(app)
-db.init_app(app)
+    #initialize the application
+    app = Flask(__name__)
 
-from app import views
+    #set up the app configuration
+    app.config.from_object(config_options[config_name])
+
+    #register the Blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+
+    #initialize flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+
+    return app
