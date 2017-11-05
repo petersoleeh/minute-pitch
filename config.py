31c71d84d7eef3a09c1e2b467d1176644082586f
peter soleeh <petersoleeh@gmail.com>
2017-11-05 15:15:56 +0300

    deploy2

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://soleeh:soleeh..@localhost/minutepitch'
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://soleeh:soleeh..@localhost/minutepitch'
     SECRET_KEY =os.environ.get('SECRET_KEY')
 
 class ProdConfig(Config):
